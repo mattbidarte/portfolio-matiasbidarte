c38875775c359376b1741b4904f5389fe34bdaf6
Matías Bidarte <matias.bidarte@outlook.com>
2022-08-17 15:06:44 -0300

    sobre mi terminando

--- js/script.js
+++ js/script.js
@@ -5,3 +5,7 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+document.querySelectorAll(".header__content-navbar-item").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
